e85c07015edec0091823e2e9d0a0426488dc575e
Yekta <yekta.senay@zeneg.de>
2023-07-02 01:53:20 +0200

    init

---
+++ src/conf/db.ts
@@ -0,0 +1,8 @@
+export function db(){
+    return {
+        "host":"4707.webhost-05.my-host.network",
+        "user":"s4703_notenapp_restapi",
+        "pass":"X8n3in~89",
+        "db":"s4703_notenapp_restapi"
+    }
+}
---
+++ src/func/students/create.ts
@@ -0,0 +1,47 @@
+import mysql from "mysql";
+
+export function createStudent(body, authtoken, db){
+
+    let conn = mysql.createConnection({
+        host: db['host'],
+        user: db['user'],
+        password: db['pass'],
+        database: db['db']
+
+    })
+    let sql = "SELECT * FROM `students` WHERE `id` = " + id
+    let res = "";
+
+    conn.query(sql,  (error, result)=>{
+        result = result[0]
+        console.log(result['id'])
+    })
+
+    console.log(res)
+
+
+    try {
+        let authdata = atob(authtoken.slice(6)).split(":")
+        let username = authdata[0]
+        let password = authdata[1]
+        return "function getStudent() | username: " + username + " | password: " + password
+    } catch (error) {
+        return JSON.stringify(
+            {
+                "error":{
+                    "code":1000,
+                    "message":"Error while parsing Username and Password",
+                    "description": "Perhaps the authtoken provided from the request is not valid."
+                },
+                "data":{
+                    "body":body,
+                    "authtoken":authtoken,
+                    "function":"getStudent()"
+                }
+            }
+        )
+    }
+
+
+    return "function createStudent()";
+}
